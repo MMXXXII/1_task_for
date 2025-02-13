e306a3921d436975d937076b9d35474ee0a91da0
MMXXII <perfilev621@gmail.com>
2025-02-13 12:36:00 +0800

    Добавлены классы + добавлен файл с тестами

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,39 +1,67 @@
 ﻿using System;
-using System.Runtime.Serialization.Formatters;
 
-
-int a;
-while (true)
+public class StudentGrades
 {
-    Console.Write("Введите количество детей в классе: ");
-    if (int.TryParse(Console.ReadLine(), out a) && a > 0)
-        break;
-    Console.WriteLine("Ошибка! Введите число больше 0");
+    private int[] gradeCount;
 
-
-
-}
-
-int[] gradeCount = new int[6];
-
-for (int i = 1; i <= a; i++)
-{
-    Console.Write("Введите имя ученика: ");
-    string name = Convert.ToString(Console.ReadLine());
-
-    Console.Write("Введите оценку ученика: ");
-    int grade;
-    while (true)
+    public StudentGrades()
     {
-        if (int.TryParse(Console.ReadLine(), out grade) && grade >= 2 && grade <= 5)
-            break;
-
-        Console.WriteLine("Ошибка! Введите корректную оценку (от 2 до 5).");
+        gradeCount = new int[6]; // Индексы 2-5 используются для подсчёта оценок
     }
-    gradeCount[grade]++;
+
+    public void AddGrade(int grade)
+    {
+        if (grade < 2 || grade > 5)
+            throw new ArgumentException("Оценка должна быть от 2 до 5.");
+
+        gradeCount[grade]++;
+    }
+
+    public int GetGradeCount(int grade)
+    {
+        if (grade < 2 || grade > 5)
+            throw new ArgumentException("Оценка должна быть от 2 до 5.");
+
+        return gradeCount[grade];
+    }
 }
 
-for (int i = 2; i <= 5; i++)
+class Program
 {
-    Console.WriteLine($"Оценка {i}: {gradeCount[i]} раз");
+    static void Main()
+    {
+        int studentCount;
+        while (true)
+        {
+            Console.Write("Введите количество детей в классе: ");
+            if (int.TryParse(Console.ReadLine(), out studentCount) && studentCount > 0)
+                break;
+
+            Console.WriteLine("Ошибка! Введите число больше 0.");
+        }
+
+        StudentGrades studentGrades = new StudentGrades();
+
+        for (int i = 1; i <= studentCount; i++)
+        {
+            Console.Write("Введите имя ученика: ");
+            string name = Console.ReadLine();
+
+            int grade;
+            while (true)
+            {
+                Console.Write("Введите оценку ученика: ");
+                if (int.TryParse(Console.ReadLine(), out grade) && grade >= 2 && grade <= 5)
+                    break;
+
+                Console.WriteLine("Ошибка! Введите корректную оценку (от 2 до 5).");
+            }
+            studentGrades.AddGrade(grade);
+        }
+
+        for (int i = 2; i <= 5; i++)
+        {
+            Console.WriteLine($"Оценка {i}: {studentGrades.GetGradeCount(i)} раз");
+        }
+    }
 }
