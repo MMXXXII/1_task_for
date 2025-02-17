efc79778344485283f3db3da0dd371f0c8ee5b8b
MMXXII <perfilev621@gmail.com>
2025-02-17 19:42:58 +0800

    изменен код и тесты

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,65 +1,47 @@
-﻿using System;
+﻿//Известны оценки каждого из учеников класса. Посчитать количество пятерок, количество четверок,
+//количество троек и количество двоек.
+using System;
 
 public class StudentGrades
 {
-    private int[] gradeCount = new int[6]; // Храним количество оценок (от 2 до 5)
+    private readonly Dictionary<int, int> gradeCount = new() { { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 } };
 
-    // Метод для добавления оценки
     public void AddGrade(int grade)
     {
-        if (grade < 2 || grade > 5)
-            throw new ArgumentException("Оценка должна быть от 2 до 5");
-
-        gradeCount[grade]++;
+        if (gradeCount.ContainsKey(grade))
+            gradeCount[grade]++;
     }
 
-    // Метод для получения количества определённой оценки
-    public int GetGradeCount(int grade)
-    {
-        if (grade < 2 || grade > 5)
-            throw new ArgumentException("Оценка должна быть от 2 до 5");
-
-        return gradeCount[grade];
-    }
+    public int GetGradeCount(int grade) => gradeCount.GetValueOrDefault(grade, 0);
 }
-
 
 class Program
 {
     static void Main()
     {
-        int studentCount;
-        while (true)
+        Console.Write("Введите количество учеников: ");
+        if (!int.TryParse(Console.ReadLine(), out int studentCount) || studentCount <= 0)
         {
-            Console.Write("Введите количество детей в классе: ");
-            if (int.TryParse(Console.ReadLine(), out studentCount) && studentCount > 0)
-                break;
-
             Console.WriteLine("Ошибка! Введите число больше 0.");
+            return;
         }
 
-        StudentGrades studentGrades = new StudentGrades();
+        StudentGrades studentGrades = new();
 
-        for (int i = 1; i <= studentCount; i++)
+        for (int i = 0; i < studentCount; i++)
         {
-            Console.Write("Введите имя ученика: ");
-            string name = Console.ReadLine();
-
-            int grade;
-            while (true)
-            {
-                Console.Write("Введите оценку ученика: ");
-                if (int.TryParse(Console.ReadLine(), out grade) && grade >= 2 && grade <= 5)
-                    break;
-
+            Console.Write("Введите оценку ученика: ");
+            if (int.TryParse(Console.ReadLine(), out int grade))
+                studentGrades.AddGrade(grade);
+            else
                 Console.WriteLine("Ошибка! Введите корректную оценку (от 2 до 5).");
-            }
-            studentGrades.AddGrade(grade);
         }
 
-        for (int i = 2; i <= 5; i++)
+        foreach (var grade in studentGrades.GetType()
+                                           .GetField("gradeCount", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                                           ?.GetValue(studentGrades) as Dictionary<int, int> ?? new())
         {
-            Console.WriteLine($"Оценка {i}: {studentGrades.GetGradeCount(i)} раз");
+            Console.WriteLine($"Оценка {grade.Key}: {grade.Value} раз");
         }
     }
 }
--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System;
 
 namespace TestProject2
 {
@@ -13,45 +12,41 @@ namespace TestProject2
             studentGrades = new StudentGrades();
         }
 
-        // Проверяем, что корректные оценки добавляются правильно
         [Test]
-        public void AddGrade_ValidGrades_IncreasesCount()
+        public void AddGrade_ValidGrade3_IncreasesCount()
         {
             studentGrades.AddGrade(3);
             studentGrades.AddGrade(3);
+
+            Assert.AreEqual(2, studentGrades.GetGradeCount(3));
+        }
+
+        [Test]
+        public void AddGrade_ValidGrade5_IncreasesCount()
+        {
             studentGrades.AddGrade(5);
 
-            Assert.AreEqual(2, studentGrades.GetGradeCount(3)); // Оценка 3 встречается 2 раза
-            Assert.AreEqual(1, studentGrades.GetGradeCount(5)); // Оценка 5 встречается 1 раз
-            Assert.AreEqual(0, studentGrades.GetGradeCount(4)); // Оценка 4 не добавлялась
+            Assert.AreEqual(1, studentGrades.GetGradeCount(5));
         }
 
-        // Проверяем, что метод GetGradeCount не ломается на пустых данных
         [Test]
-        public void GetGradeCount_NoGrades_ReturnsZero()
+        public void GetGradeCount_Grade4_NoGrades_ReturnsZero()
         {
-            Assert.AreEqual(0, studentGrades.GetGradeCount(2));
-            Assert.AreEqual(0, studentGrades.GetGradeCount(3));
             Assert.AreEqual(0, studentGrades.GetGradeCount(4));
-            Assert.AreEqual(0, studentGrades.GetGradeCount(5));
         }
 
-        // Проверяем, что метод AddGrade отказывается принимать некорректные значения
         [Test]
-        public void AddGrade_InvalidGrades_ThrowsException()
+        public void AddGrade_InvalidGrade1_DoesNotChangeCount()
         {
-            Assert.Throws<ArgumentException>(() => studentGrades.AddGrade(1)); // Меньше 2
-            Assert.Throws<ArgumentException>(() => studentGrades.AddGrade(6)); // Больше 5
-            Assert.Throws<ArgumentException>(() => studentGrades.AddGrade(0)); // Нулевая оценка
-            Assert.Throws<ArgumentException>(() => studentGrades.AddGrade(-3)); // Отрицательная оценка
+            studentGrades.AddGrade(1);
+
+            Assert.AreEqual(0, studentGrades.GetGradeCount(1));
         }
 
-        // Проверяем, что метод GetGradeCount тоже выбрасывает исключение для некорректных оценок
         [Test]
-        public void GetGradeCount_InvalidGrade_ThrowsException()
+        public void GetGradeCount_InvalidGrade6_ReturnsZero()
         {
-            Assert.Throws<ArgumentException>(() => studentGrades.GetGradeCount(1));
-            Assert.Throws<ArgumentException>(() => studentGrades.GetGradeCount(6));
+            Assert.AreEqual(0, studentGrades.GetGradeCount(6));
         }
     }
 }
